800a5555750f122d839e325977b7e22b2324e2fb
joshgreiff <josh@greiff.org>
2022-04-11 15:49:30 -0400

    added mock and tests

---
+++ __tests__/Player.test.js
@@ -0,0 +1,12 @@
+const { default: expectationResultFactory } = require('jest-jasmine2/build/expectationResultFactory');
+const Player = require('../logic/Player')
+
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave')
+    expect(player.health).toEqual(expect.any(Number))
+    expect(player.strength).toEqual(expect.any(Number))
+    expect(player.agility).toEqual(expect.any(Number))
+})
